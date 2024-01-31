520d89b95a569483bb1eebe500731553ce52ff41
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-01-31 20:05:40 +0530

    updated Lab1

---
+++ Lab1.sh
@@ -0,0 +1,13 @@
+echo "Enter your Name:"
+read $Name
+echo "Enter your Semester:"
+read $Semester
+echo "Enter your Batch:"
+read $Batch
+echo "Enter your Roll Number:"
+read $Rollno
+
+echo "Name: " $Name
+echo "Semester: " $Semester
+echo "Batch: " $Batch
+echo "Roll Number: " $Rollno
